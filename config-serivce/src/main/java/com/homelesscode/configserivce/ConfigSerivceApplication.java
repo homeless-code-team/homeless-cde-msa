8319d3f5ab6ef44a523d0c485ecad1caba427fb4
bildno <gingu33@naver.com>
2024-12-24 17:07:25 +0900

    유레카 구조

---
+++ config-serivce/src/main/java/com/homelesscode/configserivce/ConfigSerivceApplication.java
@@ -0,0 +1,13 @@
+package com.homelesscode.configserivce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigSerivceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigSerivceApplication.class, args);
+    }
+
+}
