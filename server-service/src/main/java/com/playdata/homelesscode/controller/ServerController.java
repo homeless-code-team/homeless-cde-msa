622e8468a9be6740cf42c7dd48b80b5e4917d2fd
bildno <gingu33@naver.com>
2024-12-26 11:41:14 +0900

    server-service api 수정

--- server-service/src/main/java/com/playdata/homelesscode/controller/ServerController.java
+++ server-service/src/main/java/com/playdata/homelesscode/controller/ServerController.java
@@ -17,6 +17,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -27,8 +28,8 @@ public class ServerController {
 
     private final ServerService serverService;
 
-    @PutMapping("/servers")
-    public ResponseEntity<?> createServer(ServerCreateDto dto) {
+    @PostMapping("/servers")
+    public ResponseEntity<?> createServer(ServerCreateDto dto) throws IOException {
 
         Server result = serverService.createServer(dto);
 
@@ -58,7 +59,7 @@ public class ServerController {
     }
 
 
-    @PutMapping("/channels")
+    @PostMapping("/channels")
     public ResponseEntity<?> createChannel(ChannelCreateDto dto) {
 
         Channel result = serverService.createChannel(dto);
@@ -86,7 +87,7 @@ public class ServerController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @PutMapping("/boards")
+    @PostMapping("/boards")
     public ResponseEntity<?> createBoard(@RequestBody BoardCreateDto dto){
         Board result = serverService.createBoard(dto);
 
