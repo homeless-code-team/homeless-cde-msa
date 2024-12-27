7ab857a3528576528bd1ada6635a56028ddb3852
bildno <gingu33@naver.com>
2024-12-27 14:23:00 +0900

    config-service, gateway-service, discovery-service 생성

--- config-serivce/src/main/java/com/homelesscode/configserivce/ConfigSerivceApplication.java
+++ config-serivce/src/main/java/com/homelesscode/configserivce/ConfigSerivceApplication.java
@@ -1,10 +1,15 @@
 package com.homelesscode.configserivce;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigSerivceApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigSerivceApplication.class, args);
