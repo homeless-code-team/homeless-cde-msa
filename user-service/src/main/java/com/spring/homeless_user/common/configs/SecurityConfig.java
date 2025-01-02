3d217a44f9b5dbd9be00f87bc8760d322fd78313
Si <tlgyrjs@naver.com>
2025-01-02 12:31:27 +0900

    user fifinish

--- user-service/src/main/java/com/spring/homeless_user/common/configs/SecurityConfig.java
+++ user-service/src/main/java/com/spring/homeless_user/common/configs/SecurityConfig.java
@@ -32,10 +32,9 @@ public class SecurityConfig {
                 .cors(Customizer.withDefaults()) // YAML 설정과 통합
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/api/v1/users/sign-up", "/api/v1/users/sign-in", "/api/v1/users/confirm", "/api/v1/users/duplicate","/api/v1/users/refresh-token").permitAll()
+                    auth.requestMatchers("/api/v1/users/sign-up", "/api/v1/users/sign-in", "/api/v1/users/confirm", "/api/v1/users/duplicate").permitAll()
                             .anyRequest().authenticated();
                 })
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling(exception -> exception.authenticationEntryPoint(errorEntryPoint));
 
         return http.build();
