3c34ac20034c3f350e5f0bb5bdfe03ce92a7c221
Si <tlgyrjs@naver.com>
2025-01-09 10:45:51 +0900

    2차 캐싱 적용 완료 정보수정에서 이미지만 따로 분리 완료

---
+++ friends-server/src/main/java/com/spring/homelesscode/friends_server/FriendsServerApplication.java
@@ -0,0 +1,13 @@
+package com.spring.homelesscode.friends_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriendsServerApplication.class, args);
+    }
+
+}
---
+++ friends-server/src/test/java/com/spring/homelesscode/friends_server/FriendsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.homelesscode.friends_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
