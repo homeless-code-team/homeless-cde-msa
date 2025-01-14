7f5b17d33bc884531038813f06ce238942da21b4
Chani's <psc0729@naver.com>
2025-01-14 10:32:29 +0900

    fix. 필요없는 import문 제거

--- chat-service/src/main/java/com/homeless/chatservice/repository/ChatMessageCustomRepository.java
+++ chat-service/src/main/java/com/homeless/chatservice/repository/ChatMessageCustomRepository.java
@@ -1,7 +1,5 @@
 package com.homeless.chatservice.repository;
 
-import com.homeless.chatservice.entity.ChatMessage;
-
 public interface ChatMessageCustomRepository {
     void updateContent(String id, String content);
 }
--- server-service/src/main/java/com/playdata/homelesscode/client/ChatServiceClient.java
+++ server-service/src/main/java/com/playdata/homelesscode/client/ChatServiceClient.java
@@ -3,7 +3,6 @@ package com.playdata.homelesscode.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name = "chat-service")
 public interface ChatServiceClient {
