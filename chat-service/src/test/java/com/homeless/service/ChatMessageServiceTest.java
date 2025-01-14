45b2c0f5f2c12f353e0971857156fafc7c3d32f6
Chani's <psc0729@naver.com>
2025-01-14 09:18:14 +0900

    test. 메시지생성, 메시지삭제 테스트 구현

--- chat-service/src/test/java/com/homeless/service/ChatMessageServiceTest.java
+++ chat-service/src/test/java/com/homeless/service/ChatMessageServiceTest.java
@@ -10,11 +10,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
+@Transactional
 public class ChatMessageServiceTest {
 
     @Mock
@@ -24,21 +27,41 @@ public class ChatMessageServiceTest {
     private ChatHttpService chatHttpService;
 
     @Test
-    public void testCreateChatMessage() {
-        ChatMessageCreateCommand command = new ChatMessageCreateCommand(1L, 1L,"안녕" ,"user1");
+    @Rollback
+    public void 메시지생성테스트() {
+        ChatMessageCreateCommand command = new ChatMessageCreateCommand("server-id", "channel-id","guest123","안녕" ,"user1");
         ChatMessage savedMessage = ChatMessage.builder()
-                .id("123")
-                .channelId(1L)
+                .id("testChatId")
+                .channelId("test-channel")
                 .content("Hello")
                 .writer("user1")
                 .timestamp(System.currentTimeMillis())
                 .build();
 
-        when(chatMessageRepository.save(any(ChatMessage.class))).thenReturn(savedMessage);
+        //when(chatMessageRepository.save(any(ChatMessage.class))).thenReturn(savedMessage);
 
         String chatId = chatHttpService.createChatMessage(command);
 
         assertNotNull(chatId);
-        assertEquals("123", chatId);
+        assertEquals("testChatId", chatId);
+    }
+
+    @Test
+    @Rollback
+    void 메시지삭제테스트() {
+        // given
+        ChatMessageCreateCommand command = new ChatMessageCreateCommand("server-id", "channel-id","guest123","안녕" ,"user1");
+        ChatMessage savedMessage = ChatMessage.builder()
+                .id("123")
+                .channelId("test-channel")
+                .content("Hello")
+                .writer("user1")
+                .timestamp(System.currentTimeMillis())
+                .build();
+        // when
+        String chatId = chatHttpService.createChatMessage(command);
+        chatHttpService.deleteMessage(chatId);
+        // then
+        assertNull(chatId);
     }
 }
