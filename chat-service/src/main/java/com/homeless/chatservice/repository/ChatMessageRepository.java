93a38424b3cc7652cac59b92a76c54f61d9d4f9c
Chani's <psc0729@naver.com>
2025-01-16 12:33:47 +0900

    채팅 기록 불러오기 페이지네이션

--- chat-service/src/main/java/com/homeless/chatservice/repository/ChatMessageRepository.java
+++ chat-service/src/main/java/com/homeless/chatservice/repository/ChatMessageRepository.java
@@ -4,15 +4,18 @@ package com.homeless.chatservice.repository;
 
 import com.homeless.chatservice.entity.ChatMessage;
 import org.bson.types.ObjectId;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface ChatMessageRepository extends MongoRepository<ChatMessage, String>,ChatMessageCustomRepository {
 
-    List<ChatMessage> findByChannelId(String channelId);  // 방 번호로 메시지 찾기
+    //Page<ChatMessage> findByChannelId(String channelId, Pageable pageable);  // 방 번호로 메시지 찾기
     Optional<ChatMessage> findById(ObjectId chatId); //
     void deleteChatMessageByChannelId(String channelId);
+    Page<ChatMessage> findByChannelIdAndIdGreaterThanOrderByTimestampDesc(String channelId, ObjectId id, Pageable pageable);
 
+    Page<ChatMessage> findByChannelIdOrderByTimestampDesc(String channelId, Pageable pageable);
 }
