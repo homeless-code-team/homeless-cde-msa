06df3b4547a8c126d6cf1ab6b153a78643b542ab
bildno <gingu33@naver.com>
2025-01-21 12:06:16 +0900

    server 임시 커밋

--- server-service/src/main/java/com/playdata/homelesscode/controller/ServerController.java
+++ server-service/src/main/java/com/playdata/homelesscode/controller/ServerController.java
@@ -1,28 +1,12 @@
 package com.playdata.homelesscode.controller;
 
 import com.playdata.homelesscode.common.dto.CommonResDto;
-import com.playdata.homelesscode.dto.boardList.BoardListCreateDto;
-import com.playdata.homelesscode.dto.boardList.BoardListUpdateDto;
-import com.playdata.homelesscode.dto.boards.BoardCreateDto;
-import com.playdata.homelesscode.dto.boards.BoardSearchDto;
-import com.playdata.homelesscode.dto.boards.BoardUpdateDto;
-import com.playdata.homelesscode.dto.channel.ChannelCreateDto;
-import com.playdata.homelesscode.dto.channel.ChannelResponseDto;
-import com.playdata.homelesscode.dto.channel.ChannelUpdateDto;
-import com.playdata.homelesscode.dto.server.ServerCreateDto;
-import com.playdata.homelesscode.dto.server.ServerDto;
-import com.playdata.homelesscode.dto.server.ServerResponseDto;
-import com.playdata.homelesscode.dto.user.UserResponseDto;
-import com.playdata.homelesscode.entity.Board;
-import com.playdata.homelesscode.entity.BoardList;
-import com.playdata.homelesscode.entity.Channel;
+import com.playdata.homelesscode.dto.server.*;
+import com.playdata.homelesscode.dto.user.UserReponseInRoleDto;
 import com.playdata.homelesscode.entity.Server;
 import com.playdata.homelesscode.service.ServerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -84,145 +68,40 @@ public class ServerController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-
-    // 채널 생성
-    @PostMapping("/channels")
-    public ResponseEntity<?> createChannel(ChannelCreateDto dto) {
-
-        Channel result = serverService.createChannel(dto);
-
-        CommonResDto resDto = new CommonResDto(HttpStatus.OK, "채널 생성 성공", result);
-
-        return new ResponseEntity<>(resDto, HttpStatus.OK);
-    }
-
-    // 채널 목록 조회
-    @GetMapping("/channels")
-    public ResponseEntity<?> getChannel(@RequestParam String id) {
-
-        List<ChannelResponseDto> result = serverService.getChannel(id);
-
-        CommonResDto resDto = new CommonResDto(HttpStatus.OK, "조회 성공", result);
-
-        return new ResponseEntity<>(resDto, HttpStatus.OK);
-    }
-
-    // 채널 삭제
-    @DeleteMapping("/channels")
-    public ResponseEntity<?> deleteChannel(@RequestParam String id) {
-
-        serverService.deleteChannel(id);
-
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    // 새널 수정
-    @PutMapping("/channels")
-    public ResponseEntity<?> updateChannel(ChannelUpdateDto dto) {
-        Channel result = serverService.updateChannel(dto);
-
-        CommonResDto resDto = new CommonResDto(HttpStatus.OK, "수정성공", result);
-
-        return new ResponseEntity<>(resDto, HttpStatus.OK);
-
-    }
-
-    // 게시판 생성
-    @PostMapping("/boardList")
-    public ResponseEntity<?> createBoardList(BoardListCreateDto dto) {
-        BoardList result = serverService.createBoardList(dto);
-
-        CommonResDto resDto = new CommonResDto(HttpStatus.OK, "생성 성공", result);
-
-        return new ResponseEntity<>(resDto, HttpStatus.OK);
-
-    }
-
-    // 게시판 조회
-    @GetMapping("/boardList")
-    public ResponseEntity<?> getBoardList(@RequestParam String id) {
-
-        List<BoardList> result = serverService.getBoardList(id);
-
-        CommonResDto resDto = new CommonResDto(HttpStatus.OK, "조회 성공", result);
-
-        return new ResponseEntity<>(resDto, HttpStatus.OK);
-    }
-
-    // 게시판 수정
-    @PutMapping("/boardList")
-    public ResponseEntity<?> updateBoardList(BoardListUpdateDto dto) {
-
-        BoardList board = serverService.updateBoardList(dto);
-
-        CommonResDto resDto = new CommonResDto(HttpStatus.OK, "수정 성공", board);
-
-        return new ResponseEntity<>(resDto, HttpStatus.OK);
-    }
-
-    // 게시판 삭제
-    @DeleteMapping("/boardList")
-    public ResponseEntity<?> deleteBoardList(@RequestParam String id) {
-
-        log.info("삭제 컨트롤러에여 {} ,", id);
-
-        serverService.deleteBoardList(id);
-
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    // 게시글 생성
-    @PostMapping("/boards")
-    public ResponseEntity<?> createBoards(BoardCreateDto dto) {
-
-        serverService.createBoard(dto);
-
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    // 게시글 조회
-    @GetMapping("/boards")
-    public ResponseEntity<?> getBoards(BoardSearchDto dto, Pageable pageable) {
-
-        Page<Board> result = serverService.getBoard(dto,pageable);
-
-        CommonResDto resDto = new CommonResDto<>(HttpStatus.OK, "조회 성공", result);
-
-        return new ResponseEntity<>(resDto, HttpStatus.OK);
-    }
-
-    @DeleteMapping("/boards")
-    public ResponseEntity<?> deleteBoards(@RequestParam String id) {
-
-        serverService.deleteBoard(id);
-
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-
-    @PutMapping("/boards")
-    public ResponseEntity<?> updateBoards(BoardUpdateDto dto) {
-
-        serverService.updateBoard(dto);
-
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-
+    // 서버 참여 유저 조회
     @GetMapping("/userList")
-    public CommonResDto<List<UserResponseDto>> getUserList(@RequestParam String id) {
+    public CommonResDto<List<UserReponseInRoleDto>> getUserList(@RequestParam String id ) {
 
 
-        List<UserResponseDto> userList = serverService.getUserList(id);
-        log.info("여기는 서버 컨트롤러 {}", userList);
+        List<UserReponseInRoleDto> userList = serverService.getUserList(id);
 
-        CommonResDto<List<UserResponseDto>> List = new CommonResDto<>(HttpStatus.OK, "조회성공", userList);
+
+        CommonResDto<List<UserReponseInRoleDto>> List = new CommonResDto<>(HttpStatus.OK, "조회성공", userList);
 
 
 
         return List;
 
     }
+
+    // 서버 권한 변경
+    @PutMapping("/userRole")
+    public ResponseEntity<?> changeRole(@RequestBody ChangeRoleDto dto){
+        serverService.changeRole(dto);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    // 유저 추방
+    @DeleteMapping("/resign")
+    public ResponseEntity<?> resignUser(@RequestBody ResignUserDto dto){
+        serverService.resignUser(dto);
+        
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
+
     ////////////////////////////////////////////// 서버관리 /////////////////////////////////////////////////////////////////
 
     // 서버 추가요청
