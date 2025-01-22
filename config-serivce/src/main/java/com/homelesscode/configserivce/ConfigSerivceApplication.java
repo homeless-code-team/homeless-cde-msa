6bc656dc99f013027bd6009c6ba3e187e44b2e26
Chani's <psc0729@naver.com>
2025-01-22 19:18:41 +0900

    컨피그 서비스 코드 리포매팅

--- config-serivce/src/main/java/com/homelesscode/configserivce/ConfigSerivceApplication.java
+++ config-serivce/src/main/java/com/homelesscode/configserivce/ConfigSerivceApplication.java
@@ -1,6 +1,5 @@
 package com.homelesscode.configserivce;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
