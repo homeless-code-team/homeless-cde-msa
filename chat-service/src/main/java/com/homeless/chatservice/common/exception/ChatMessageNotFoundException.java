28f2f192ec758b2cbcc68ced93c9651b60323927
Chani's <psc0729@naver.com>
2025-01-23 16:38:34 +0900

    리팩터링

--- chat-service/src/main/java/com/homeless/chatservice/common/exception/ChatMessageNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.homeless.chatservice.common.exception;
-
-public class ChatMessageNotFoundException extends RuntimeException {
-    public ChatMessageNotFoundException(String message) {
-        super(message);
-    }
-}
