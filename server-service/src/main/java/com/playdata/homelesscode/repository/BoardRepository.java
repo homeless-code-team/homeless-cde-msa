71298f69e437e3527ae031246492bb5396eed227
Chani's <psc0729@naver.com>
2025-01-24 17:12:08 +0900

    보드,보드리스트 삭제시 mongoDB chatMessages 삭제 구현.

--- server-service/src/main/java/com/playdata/homelesscode/repository/BoardRepository.java
+++ server-service/src/main/java/com/playdata/homelesscode/repository/BoardRepository.java
@@ -1,9 +1,14 @@
 package com.playdata.homelesscode.repository;
 
 import com.playdata.homelesscode.entity.Board;
+import com.playdata.homelesscode.entity.BoardList;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 
 public interface BoardRepository extends JpaRepository<Board, String> {
@@ -11,4 +16,6 @@ public interface BoardRepository extends JpaRepository<Board, String> {
     Page<Board> findByBoardListIdOrderByCreateAtDesc(String id, Pageable pageable);
 
     Page<Board> findByBoardListIdAndTitleContainingOrderByCreateAtDesc(String boardId, String title, Pageable pageable);
+
+    List<Board> findByBoardListId(String boardId);
 }
