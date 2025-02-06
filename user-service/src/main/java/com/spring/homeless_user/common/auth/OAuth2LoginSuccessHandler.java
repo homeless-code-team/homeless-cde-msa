05e4cdf41daeef1e4c8eab6939cc3942c6fe0c04
Si <tlgyrjs@naver.com>
2025-02-06 19:19:46 +0900

    gateway 이전으로 다시 돌아감

--- user-service/src/main/java/com/spring/homeless_user/common/auth/OAuth2LoginSuccessHandler.java
+++ user-service/src/main/java/com/spring/homeless_user/common/auth/OAuth2LoginSuccessHandler.java
@@ -5,32 +5,49 @@ import com.spring.homeless_user.user.repository.UserRepository;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @Component
 public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
     private final JwtTokenProvider jwtTokenProvider;
     private final UserRepository userRepository;
+    private final RedisTemplate<String, String> loginTemplate;
 
-    public OAuth2LoginSuccessHandler(JwtTokenProvider jwtTokenProvider, UserRepository userRepository) {
+    public OAuth2LoginSuccessHandler(JwtTokenProvider jwtTokenProvider,
+                                     UserRepository userRepository,
+                                     @Qualifier("login") RedisTemplate<String, String> loginTemplate) {
         this.jwtTokenProvider = jwtTokenProvider;
         this.userRepository = userRepository;
+        this.loginTemplate = loginTemplate;
     }
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        log.info("onAuthenticationSuccess");
+        log.info("authentication: {}", authentication);
+
         String email = authentication.getName(); // OAuth 로그인한 사용자 이메일
+
         User user = userRepository.findByEmail(email).orElseThrow(() -> new IllegalArgumentException("유저 정보를 찾을 수 없습니다."));
 
         // 🔹 JWT 발급
         String jwtToken = jwtTokenProvider.accessToken(user.getEmail(), user.getNickname(), user.getId());
+        log.info("jwtToken: {}", jwtToken);
 
+        String refreshToken = jwtTokenProvider.refreshToken(user.getEmail(), user.getNickname());
+        log.info("refreshToken: {}", refreshToken);
+        loginTemplate.opsForValue().set(user.getEmail(), refreshToken, 14, TimeUnit.DAYS);
         // 🔥 클라이언트로 리디렉트할 URL 생성
         String redirectUrl = UriComponentsBuilder.fromUriString("https://homelesscode.shop/callback")
                 .queryParam("token", jwtToken)
--- user-service/src/main/java/com/spring/homeless_user/user/service/OAuth2UserServiceImpl.java
+++ user-service/src/main/java/com/spring/homeless_user/user/service/OAuth2UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.spring.homeless_user.user.service;
 import com.spring.homeless_user.common.auth.JwtTokenProvider;
 import com.spring.homeless_user.user.entity.User;
 import com.spring.homeless_user.user.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
@@ -12,7 +13,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 import java.util.UUID;
-
+@Slf4j
 @Service
 public class OAuth2UserServiceImpl implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
 
@@ -22,8 +23,13 @@ public class OAuth2UserServiceImpl implements OAuth2UserService<OAuth2UserReques
         this.userRepository = userRepository;
     }
 
+
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        log.info(userRequest.getClientRegistration().getRegistrationId());
+        log.info(userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUri());
+        log.info(userRequest.getAccessToken().getTokenValue());
+
         OAuth2User oauth2User = new DefaultOAuth2UserService().loadUser(userRequest);
         String email = oauth2User.getAttribute("email");
         String name = oauth2User.getAttribute("name");
