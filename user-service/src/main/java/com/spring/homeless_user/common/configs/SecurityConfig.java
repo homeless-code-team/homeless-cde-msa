46a6212ba419cc7fcd70de8a0ee4f8fd04ac46e8
Si <tlgyrjs@naver.com>
2025-02-06 18:33:45 +0900

    gateway 이전으로 다시 돌아감

--- user-service/src/main/java/com/spring/homeless_user/common/configs/SecurityConfig.java
+++ user-service/src/main/java/com/spring/homeless_user/common/configs/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.spring.homeless_user.common.configs;
 
+import com.spring.homeless_user.common.auth.JwtAuthFilter;
 import com.spring.homeless_user.common.auth.OAuth2LoginSuccessHandler;
 import com.spring.homeless_user.common.dto.ErrorEntryPoint;
 import com.spring.homeless_user.user.service.OAuth2UserServiceImpl;
@@ -12,6 +13,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -22,14 +24,16 @@ public class SecurityConfig {
     private final OAuth2UserServiceImpl oAuth2UserService;
     private final SecurityPropertiesUtil securityPropertiesUtil; // ✅ SecurityPropertiesUtil 추가
     private final ErrorEntryPoint errorEntryPoint;
+    private final JwtAuthFilter jwtAuthFilter;
 
     public SecurityConfig(OAuth2LoginSuccessHandler oAuth2SuccessHandler,
                           OAuth2UserServiceImpl oAuth2UserService,
-                          SecurityPropertiesUtil securityPropertiesUtil, ErrorEntryPoint errorEntryPoint) {
+                          SecurityPropertiesUtil securityPropertiesUtil, ErrorEntryPoint errorEntryPoint, JwtAuthFilter jwtAuthFilter) {
         this.oAuth2SuccessHandler = oAuth2SuccessHandler;
         this.oAuth2UserService = oAuth2UserService;
         this.securityPropertiesUtil = securityPropertiesUtil; // ✅ 주입
         this.errorEntryPoint = errorEntryPoint;
+        this.jwtAuthFilter = jwtAuthFilter;
     }
 
     @Bean
@@ -46,6 +50,7 @@ public class SecurityConfig {
 
                     auth.anyRequest().authenticated(); // 그 외 모든 요청은 인증 필요
                 })
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling(exception -> {
                     exception.authenticationEntryPoint(errorEntryPoint);
                 })
